774bd9cf491a010cebcf47dc0b89d8ce03bb8224
YAOYELU <luyaoyejeren@gmail.com>
2019-04-25 14:02:53 +1000

    rewrite alphabeta search and getbestmove, it can works, but result is so bad

--- ass3/src/alpha_beta_search.c
+++ ass3/src/alpha_beta_search.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define BLOCK -2
-#define MAKING_3Xs 10
-#define MAKING_2Xs 1
+#define H2Os -2
+#define H3Xs 10
+#define H2Xs 1
 #define BASIC_SCORE 0
-#define MAKING_3Os -10
+#define H3Os -10
 #define AL -20000000
 #define BT 20000000
 #ifndef max
@@ -17,11 +17,10 @@
 /*
 // Heuristic:
 //version2:
-1.had 3X(i):10000
-2.had 2X(i): 10
-3.n of rows/columns/diagonses having X(i): n
-// 4.had 3O(i): -5000
-5.Blockling 3O(i): 5000
+1.had 3X(i):10
+2.had 2X(i): 1
+3.had 3O(i): -20
+4.had 2O(i): -2
 */
 //Eval version 2
 int Eval(int subboard[10]){
@@ -39,7 +38,7 @@ int Eval(int subboard[10]){
         (subboard[1] == 0 && subboard[5] == 0 && subboard[9] == 0)||
         (subboard[3] == 0 && subboard[5] == 0 && subboard[7] == 0)
     ){
-        value = value + MAKING_3Xs;
+        value = value + H3Xs;
         return value;
     }
     else if(
@@ -55,7 +54,7 @@ int Eval(int subboard[10]){
         (subboard[1] == 1 && subboard[5] == 1 && subboard[9] == 1)||
         (subboard[3] == 1 && subboard[5] == 1 && subboard[7] == 1)
     ){
-        value = value + MAKING_3Os;
+        value = value + H3Os;
         return value;
     }
     // rows
@@ -65,7 +64,7 @@ int Eval(int subboard[10]){
                 (subboard[i] ==0 && subboard[i+1] ==0 && subboard[i+2] ==2)||
                 (subboard[i] ==2 && subboard[i+1] ==0 && subboard[i+2] ==0)||
                 (subboard[i] ==0 && subboard[i+1] ==2 && subboard[i+2] ==0)
-            ){value = value + MAKING_2Xs;}
+            ){value = value + H2Xs;}
             if(
                 (subboard[i] ==0 && subboard[i+1] ==2 && subboard[i+2] ==2)||
                 (subboard[i] ==2 && subboard[i+1] ==0 && subboard[i+2] ==2)||
@@ -80,7 +79,7 @@ int Eval(int subboard[10]){
                 (subboard[i] ==0 && subboard[i+3] ==0 && subboard[i+6] ==2)||
                 (subboard[i] ==2 && subboard[i+3] ==0 && subboard[i+6] ==0)||
                 (subboard[i] ==0 && subboard[i+3] ==2 && subboard[i+6] ==0)
-            ){value = value + MAKING_2Xs;}
+            ){value = value + H2Xs;}
             if(
                 (subboard[i] ==0 && subboard[i+3] ==2 && subboard[i+6] ==2)||
                 (subboard[i] ==2 && subboard[i+3] ==0 && subboard[i+6] ==2)||
@@ -94,7 +93,7 @@ int Eval(int subboard[10]){
             (subboard[1] ==0 && subboard[5] ==0 && subboard[9] ==2)||
             (subboard[1] ==2 && subboard[5] ==0 && subboard[9] ==0)||
             (subboard[1] ==0 && subboard[5] ==2 && subboard[9] ==0)
-        ){value = value + MAKING_2Xs;}
+        ){value = value + H2Xs;}
         if(
             (subboard[1] ==0 && subboard[5] ==2 && subboard[9] ==2)||
             (subboard[1] ==2 && subboard[5] ==0 && subboard[9] ==2)||
@@ -106,7 +105,7 @@ int Eval(int subboard[10]){
             (subboard[3] ==0 && subboard[5] ==0 && subboard[7] ==2)||
             (subboard[3] ==2 && subboard[5] ==0 && subboard[7] ==0)||
             (subboard[3] ==0 && subboard[5] ==2 && subboard[7] ==0)
-        ){value = value + MAKING_2Xs;}
+        ){value = value + H2Xs;}
         if(
             (subboard[3] ==0 && subboard[5] ==2 && subboard[7] ==2)||
             (subboard[3] ==2 && subboard[5] ==0 && subboard[7] ==2)||
@@ -122,7 +121,7 @@ int Eval(int subboard[10]){
                 (subboard[i] ==1 && subboard[i+1] ==1 && subboard[i+2] ==2)||
                 (subboard[i] ==2 && subboard[i+1] ==1 && subboard[i+2] ==1)||
                 (subboard[i] ==1 && subboard[i+1] ==2 && subboard[i+2] ==1)
-            ){value = value + BLOCK;}
+            ){value = value + H2Os;}
         }
     }
     //columns
@@ -132,7 +131,7 @@ int Eval(int subboard[10]){
                 (subboard[i] ==1 && subboard[i+3] ==1 && subboard[i+6] ==2)||
                 (subboard[i] ==2 && subboard[i+3] ==1 && subboard[i+6] ==1)||
                 (subboard[i] ==1 && subboard[i+3] ==2 && subboard[i+6] ==1)
-            ){value = value + BLOCK;}
+            ){value = value + H2Os;}
         }
     }
     //diagnoses
@@ -141,14 +140,14 @@ int Eval(int subboard[10]){
             (subboard[1] ==1 && subboard[5] ==1 && subboard[9] ==2)||
             (subboard[1] ==2 && subboard[5] ==1 && subboard[9] ==1)||
             (subboard[1] ==1 && subboard[5] ==2 && subboard[9] ==1)
-        ){value = value + BLOCK;}
+        ){value = value + H2Os;}
     }
     if(subboard[3] != 1 && subboard[5] != 1 && subboard[7] != 1){
         if(
             (subboard[3] ==1 && subboard[5] ==1 && subboard[7] ==2)||
             (subboard[3] ==2 && subboard[5] ==1 && subboard[7] ==1)||
             (subboard[3] ==1 && subboard[5] ==2 && subboard[7] ==1)
-        ){value = value + BLOCK;}
+        ){value = value + H2Os;}
     }
     return value;
 }
@@ -179,18 +178,18 @@ int isFull(int board[10]){
     }
     return 1;
 }
-int alphabeta(int board[10], int depth, int alpha, int beta, int Player)
+int alphabeta(int board[10][10], int prev_move, int depth, int alpha, int beta, int Player)
 {
     // int val;
-    if(isFull(board)==1 || depth == 0){
-        return Eval(board);
+    if(isFull(board[prev_move])==1 || depth == 0){
+        return Eval(board[prev_move]);
     }
-    if(!Player){
+    if(Player == 0){
         for (int i = 1; i <10;i++){
-            if(board[i] == 2){
-                board[i] = 0;
-                alpha = max(alpha,alphabeta(board,depth-1,alpha,beta,!Player));
-                board[i] = 2;
+            if(board[prev_move][i] == 2){
+                board[prev_move][i] = 0;
+                alpha = max(alpha,alphabeta(board,i,depth-1,alpha,beta,!Player));
+                board[prev_move][i] = 2;
                 if (alpha >= beta){
                     return alpha;
                 }
@@ -200,10 +199,10 @@ int alphabeta(int board[10], int depth, int alpha, int beta, int Player)
     }
     else{
         for (int i = 1; i <10;i++){
-            if(board[i] == 2){
-                board[i] = 1;
-                beta = min(beta, alphabeta(board,depth-1,alpha,beta,!Player));
-                board[i] = 2;
+            if(board[prev_move][i] == 2){
+                board[prev_move][i] = 1;
+                beta = min(beta, alphabeta(board,i,depth-1,alpha,beta,!Player));
+                board[prev_move][i] = 2;
                 if (alpha >= beta){
                     return beta;
                 }
@@ -214,51 +213,71 @@ int alphabeta(int board[10], int depth, int alpha, int beta, int Player)
 }
 int getBestMove(int board[10][10], int prev_move, int depth, int player){
     int val;
-    int X_array[10];
-    // int O_array[10];
     int BM = 0;
-    int BS = 1000000000;
-    for(int i = 1; i< 10;i++){
-        if(board[prev_move][i] == 2){
-            board[prev_move][i] = 0;
-            val = alphabeta(board[prev_move],depth,AL,BT,!player);
-            board[prev_move][i] = 2;
-            X_array[i] = val;
-            // if(val > BS){
-            //     BS = val;
-            //     BM = i;
-            // }
-            // printf("%d\n",val);
-        }
-    }
-    //watch on next move
-    for(int i = 1; i <10; i ++){
-        if(board[prev_move][i] == 2){
-            if(X_array[i] != 0){
+    // int X_array[10];
+    // int O_array[10];
+    // int BS = -1000;
+    if(player == 0){
+        int BS = -1000;
+        for(int i = 1; i< 10;i++){
+            if(board[prev_move][i] == 2){
                 board[prev_move][i] = 0;
-                //if next move is that I win, then choose first.
-                if(Eval(board[prev_move])==10){
-                    board[prev_move][i] = 2;
-                    return i;
-                }else{
-                    // choose the worst next next move for the opponent
-                    int worst_oppent = -10000;
-                    for(int j = 1; j<10;j++){
-                        if(board[i][j] == 2){
-                            board[i][j] = 1;
-                            worst_oppent = max(worst_oppent, Eval(board[i]));
-                            board[i][j] = 2;
-                        }
-                    }
-                    board[prev_move][i] = 2;
-                    if(BS > X_array[i] - worst_oppent){
-                        BS = X_array[i] - worst_oppent;
-                        BM = i;
-                    }
+                val = alphabeta(board,i,depth,AL,BT,1);
+                board[prev_move][i] = 2;
+                // X_array[i] = val;
+                if(val > BS){
+                    BS = val;
+                    BM = i;
                 }
+                printf("%d\n",val);
+            }
+        }
+    }else
+    {
+        int BS = 1000;
+        for(int i = 1; i< 10;i++){
+            if(board[prev_move][i] == 2){
+                board[prev_move][i] = 1;
+                val = alphabeta(board,i,depth,AL,BT,0);
+                board[prev_move][i] = 2;
+                // X_array[i] = val;
+                if(val < BS){
+                    BS = val;
+                    BM = i;
+                }
+                printf("%d\n",val);
             }
         }
     }
+    
+    // //watch on next move
+    // for(int i = 1; i <10; i ++){
+    //     if(board[prev_move][i] == 2){
+    //         if(X_array[i] != 0){
+    //             board[prev_move][i] = 0;
+    //             //if next move is that I win, then choose first.
+    //             if(Eval(board[prev_move])==10){
+    //                 board[prev_move][i] = 2;
+    //                 return i;
+    //             }else{
+    //                 // choose the worst next next move for the opponent
+    //                 int worst_oppent = -10000;
+    //                 for(int j = 1; j<10;j++){
+    //                     if(board[i][j] == 2){
+    //                         board[i][j] = 1;
+    //                         worst_oppent = max(worst_oppent, Eval(board[i]));
+    //                         board[i][j] = 2;
+    //                     }
+    //                 }
+    //                 board[prev_move][i] = 2;
+    //                 if(BS > X_array[i] - worst_oppent){
+    //                     BS = X_array[i] - worst_oppent;
+    //                     BM = i;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
     return BM;
 }
 
