cd06b2b86df4501d141f014e1aeae3d33b861c79
YAOYELU <luyaoyejeren@gmail.com>
2019-04-26 20:30:54 +1000

    [ass3:reconstruct getBestMove function, it is much nicer]

--- ass3/src/alpha_beta_search.c
+++ ass3/src/alpha_beta_search.c
@@ -14,11 +14,6 @@
 #include"BinomialHeapPQ.c"
 /*
 // Heuristic:
-//version3:
-possible wins: X/O *100
-100 means X wins
-0 means O wins
-
 //version4:
 possible wins of target(having 2/3 in a row/column/diagnose)
 */
@@ -105,156 +100,6 @@ int Eval(int subboard[10],int target){
     }
     return wins*10;
 }
-//Eval version 3
-// int Eval(int subboard[10],int target){
-//     int X_wins = 0;
-//     int O_wins = 0;
-//     if(
-//         //columns
-//         (subboard[1] == 0 && subboard[4] == 0 && subboard[7] == 0)||
-//         (subboard[2] == 0 && subboard[5] == 0 && subboard[8] == 0)||
-//         (subboard[3] == 0 && subboard[6] == 0 && subboard[9] == 0)||
-//         //rows
-//         (subboard[1] == 0 && subboard[2] == 0 && subboard[3] == 0)||
-//         (subboard[4] == 0 && subboard[5] == 0 && subboard[6] == 0)||
-//         (subboard[7] == 0 && subboard[8] == 0 && subboard[9] == 0)||
-//         //diagnoses
-//         (subboard[1] == 0 && subboard[5] == 0 && subboard[9] == 0)||
-//         (subboard[3] == 0 && subboard[5] == 0 && subboard[7] == 0)
-//     ){return 100;}
-//     else if(
-//         //columns
-//         (subboard[1] == 1 && subboard[4] == 1 && subboard[7] == 1)||
-//         (subboard[2] == 1 && subboard[5] == 1 && subboard[8] == 1)||
-//         (subboard[3] == 1 && subboard[6] == 1 && subboard[9] == 1)||
-//         //rows
-//         (subboard[1] == 1 && subboard[2] == 1 && subboard[3] == 1)||
-//         (subboard[4] == 1 && subboard[5] == 1 && subboard[6] == 1)||
-//         (subboard[7] == 1 && subboard[8] == 1 && subboard[9] == 1)||
-//         //diagnoses
-//         (subboard[1] == 1 && subboard[5] == 1 && subboard[9] == 1)||
-//         (subboard[3] == 1 && subboard[5] == 1 && subboard[7] == 1)
-//     ){return 0;}
-//     else{
-//         // rows
-//         for(int i = 1; i<10;i = i+3){
-//             if(// having 2 Xs
-//                 (subboard[i] ==0 && subboard[i+1] ==0 && subboard[i+2] ==2)||
-//                 (subboard[i] ==2 && subboard[i+1] ==0 && subboard[i+2] ==0)||
-//                 (subboard[i] ==0 && subboard[i+1] ==2 && subboard[i+2] ==0)
-//             ){X_wins++;}
-//             if(// having 1 X
-//                 (subboard[i] ==0 && subboard[i+1] ==2 && subboard[i+2] ==2)||
-//                 (subboard[i] ==2 && subboard[i+1] ==0 && subboard[i+2] ==2)||
-//                 (subboard[i] ==2 && subboard[i+1] ==2 && subboard[i+2] ==0)
-//             ){X_wins++;}
-//             if(// having 2 Os
-//                 (subboard[i] ==1 && subboard[i+1] ==1 && subboard[i+2] ==2)||
-//                 (subboard[i] ==2 && subboard[i+1] ==1 && subboard[i+2] ==1)||
-//                 (subboard[i] ==1 && subboard[i+1] ==2 && subboard[i+2] ==1)
-//             ){O_wins++;}
-//             if(// having 1 O
-//                 (subboard[i] ==1 && subboard[i+1] ==2 && subboard[i+2] ==2)||
-//                 (subboard[i] ==2 && subboard[i+1] ==1 && subboard[i+2] ==2)||
-//                 (subboard[i] ==2 && subboard[i+1] ==2 && subboard[i+2] ==1)
-//             ){O_wins++;}
-//             // empty row
-//             if(subboard[i] ==2 && subboard[i+1] ==2 && subboard[i+2] == 2){
-//                 X_wins++;
-//                 O_wins++;
-//             }
-//         }
-//         //columns
-//         for(int i = 1; i<4;i++){
-//             if(// having 2 Xs
-//                 (subboard[i] ==0 && subboard[i+3] ==0 && subboard[i+6] ==2)||
-//                 (subboard[i] ==2 && subboard[i+3] ==0 && subboard[i+6] ==0)||
-//                 (subboard[i] ==0 && subboard[i+3] ==2 && subboard[i+6] ==0)
-//             ){X_wins++;}
-//             if(// having 1 X
-//                 (subboard[i] ==0 && subboard[i+3] ==2 && subboard[i+6] ==2)||
-//                 (subboard[i] ==2 && subboard[i+3] ==0 && subboard[i+6] ==2)||
-//                 (subboard[i] ==2 && subboard[i+3] ==2 && subboard[i+6] ==0)
-//             ){X_wins++;}
-//             if(// having 2 Os
-//                 (subboard[i] ==1 && subboard[i+3] ==1 && subboard[i+6] ==2)||
-//                 (subboard[i] ==2 && subboard[i+3] ==1 && subboard[i+6] ==1)||
-//                 (subboard[i] ==1 && subboard[i+3] ==2 && subboard[i+6] ==1)
-//             ){O_wins++;}
-//             if(// having 1 O
-//                 (subboard[i] ==1 && subboard[i+3] ==2 && subboard[i+6] ==2)||
-//                 (subboard[i] ==2 && subboard[i+3] ==1 && subboard[i+6] ==2)||
-//                 (subboard[i] ==2 && subboard[i+3] ==2 && subboard[i+6] ==1)
-//             ){O_wins++;}
-//             // empty row
-//             if(subboard[i] ==2 && subboard[i+1] ==2 && subboard[i+2] == 2){
-//                 X_wins++;
-//                 O_wins++;
-//             }
-//         }
-//         //diagnoses
-//         if(// having 2 Xs
-//             (subboard[1] ==0 && subboard[5] ==0 && subboard[9] ==2)||
-//             (subboard[1] ==2 && subboard[5] ==0 && subboard[9] ==0)||
-//             (subboard[1] ==0 && subboard[5] ==2 && subboard[9] ==0)
-//         ){X_wins++;}
-//         if(//having 1 X
-//             (subboard[1] ==0 && subboard[5] ==2 && subboard[9] ==2)||
-//             (subboard[1] ==2 && subboard[5] ==0 && subboard[9] ==2)||
-//             (subboard[1] ==2 && subboard[5] ==2 && subboard[9] ==0)
-//         ){X_wins++;}
-//         if(// having 2 Xs
-//             (subboard[3] ==0 && subboard[5] ==0 && subboard[7] ==2)||
-//             (subboard[3] ==2 && subboard[5] ==0 && subboard[7] ==0)||
-//             (subboard[3] ==0 && subboard[5] ==2 && subboard[7] ==0)
-//         ){X_wins++;}
-//         if(// having 1 X
-//             (subboard[3] ==0 && subboard[5] ==2 && subboard[7] ==2)||
-//             (subboard[3] ==2 && subboard[5] ==0 && subboard[7] ==2)||
-//             (subboard[3] ==2 && subboard[5] ==2 && subboard[7] ==0)
-//         ){X_wins++;}
-//         if(// having 2 Os
-//             (subboard[1] ==1 && subboard[5] ==1 && subboard[9] ==2)||
-//             (subboard[1] ==2 && subboard[5] ==1 && subboard[9] ==1)||
-//             (subboard[1] ==1 && subboard[5] ==2 && subboard[9] ==1)
-//         ){O_wins++;}
-//         if(//having 1 O
-//             (subboard[1] ==1 && subboard[5] ==2 && subboard[9] ==2)||
-//             (subboard[1] ==2 && subboard[5] ==1 && subboard[9] ==2)||
-//             (subboard[1] ==2 && subboard[5] ==2 && subboard[9] ==1)
-//         ){O_wins++;}
-//         if(// having 2 Os
-//             (subboard[3] ==1 && subboard[5] ==1 && subboard[7] ==2)||
-//             (subboard[3] ==2 && subboard[5] ==1 && subboard[7] ==1)||
-//             (subboard[3] ==1 && subboard[5] ==2 && subboard[7] ==1)
-//         ){O_wins++;}
-//         if(// having 1 O
-//             (subboard[3] ==1 && subboard[5] ==2 && subboard[7] ==2)||
-//             (subboard[3] ==2 && subboard[5] ==1 && subboard[7] ==2)||
-//             (subboard[3] ==2 && subboard[5] ==2 && subboard[7] ==1)
-//         ){O_wins++;}
-//         if(//empty
-//             subboard[1] ==2 && subboard[5] ==2 && subboard[9] ==2
-//         ){
-//             X_wins++;
-//             O_wins++;
-//         }
-//         if(//empty
-//             subboard[3] ==2 && subboard[5] ==2 && subboard[7] ==2
-//         ){
-//             X_wins++;
-//             O_wins++;
-//         }
-//         // printf("x:%d, o: %d\n",X_wins,O_wins);
-//         // int temp = ceil(X_wins*100/(O_wins+X_wins));
-//         // printf("x/o:%d\n",temp);
-//         if(X_wins == 0 && O_wins == 0){
-//             return 50;
-//         }else{
-//             return ceil(X_wins*100/(O_wins+X_wins));
-//         }
-//     }
-// }
 int Cal(int subboard[10],int target){
     int count = 0;
     for(int i = 1;i<10;i++){
@@ -432,7 +277,7 @@ int alphabeta(int board[10][10], int prev_move, int depth, int alpha, int beta, 
 }
 int getBestMove(int board[10][10], int prev_move, int depth, int player){
     int val;
-    int PerformanceOfX[10] = {0,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+    int Performance[10] = {0,-1,-1,-1,-1,-1,-1,-1,-1,-1};
     BinomialHeap * PQ1 = newHeap();
         // if(Cal(board[prev_move],2) == 9 && Cal(board[5],0) == 0){
         //     return 5;
@@ -446,72 +291,56 @@ int getBestMove(int board[10][10], int prev_move, int depth, int player){
             Insert(PQ1,val,i,0,0,0);
         }
     }
-    for(int i = PerformanceOfX[0]+1; i <10;i++){
+    for(int i = 1; i <10;i++){
         if(PQ1->smallestB == NULL){break;}
-        PerformanceOfX[i] = RemoveMin(PQ1)->TaskName;
-        PerformanceOfX[0] ++;
+        Performance[i] = RemoveMin(PQ1)->TaskName;
+        Performance[0] ++;
+        // win at this step
+        if(Blocking(board[prev_move],player,Performance[i]) == 1){
+            return Performance[i];
+        }
     }
 
-    if(PerformanceOfX[0]==1){
-        printf("1X\n");
-        return PerformanceOfX[1];
+    if(Performance[0]==1){
+        printf("1player:%d\n",player);
+        return Performance[1];
     }else{
         if(player == 0){
             //reversed visiting the array
-            for(int j= PerformanceOfX[0]; j>0 ;j--){
-                // if win after this move, then choose it first
-                //otherwise, if this move bolcks, and dont lose in next board, then choose it
-                board[prev_move][PerformanceOfX[j]] = player;
-                // win at this step
-                if(isWon(board[prev_move],player,3) == 1){
-                    board[prev_move][PerformanceOfX[j]] = 2;
-                    return PerformanceOfX[j];
+            for(int j= Performance[0]; j>0 ;j--){
+                // if this move bolcks, and dont lose in next board, then choose it
+                if(Blocking(board[prev_move],!player,Performance[j]) == 1 && isWon(board[Performance[j]],!player,2) != 1){
+                    printf("bb player:%d\n",player);
+                    return Performance[j];
                 }else{
-                    //blcking(but not really blocking)
-                    board[prev_move][PerformanceOfX[j]] = 2;
-                    if(Blocking(board[prev_move],!player,PerformanceOfX[j]) == 1 && isWon(board[PerformanceOfX[j]],!player,2) != 1){
-                        printf("bb X\n");
-                        return PerformanceOfX[j];
-                    }else{
-                        board[prev_move][PerformanceOfX[j]] = player;
-                        if(isWon(board[PerformanceOfX[j]],!player,2)!=1){
-                            board[prev_move][PerformanceOfX[j]] = 2;
-                            printf("2X\n");
-                            return PerformanceOfX[j];
-                        }
-                        board[prev_move][PerformanceOfX[j]] = 2;
+                    board[prev_move][Performance[j]] = player;
+                    if(isWon(board[Performance[j]],!player,2)!=1){
+                        board[prev_move][Performance[j]] = 2;
+                        printf("2player:%d\n",player);
+                        return Performance[j];
                     }
+                    board[prev_move][Performance[j]] = 2;
                 }
             }
         }else{
-            for(int j=1; j<= PerformanceOfX[0]; j++){
-                // if win after this move, then choose it first
-                //otherwise, if this move bolcks, and dont lose in next board, then choose it
-                board[prev_move][PerformanceOfX[j]] = player;
-                // win at this step
-                if(isWon(board[prev_move],player,3) == 1){
-                    board[prev_move][PerformanceOfX[j]] = 2;
-                    return PerformanceOfX[j];
+            for(int j=1; j<= Performance[0]; j++){
+                // if this move bolcks, and dont lose in next board, then choose it
+                if(Blocking(board[prev_move],!player,Performance[j]) == 1 && isWon(board[Performance[j]],!player,2) != 1){
+                    printf("bb player:%d\n",player);
+                    return Performance[j];
                 }else{
-                    //blcking(but not really blocking)
-                    board[prev_move][PerformanceOfX[j]] = 2;
-                    if(Blocking(board[prev_move],!player,PerformanceOfX[j]) == 1 && isWon(board[PerformanceOfX[j]],!player,2) != 1){
-                        printf("bb X\n");
-                        return PerformanceOfX[j];
-                    }else{
-                        board[prev_move][PerformanceOfX[j]] = player;
-                        if(isWon(board[PerformanceOfX[j]],!player,2)!=1){
-                            board[prev_move][PerformanceOfX[j]] = 2;
-                            printf("2X\n");
-                            return PerformanceOfX[j];
-                        }
-                        board[prev_move][PerformanceOfX[j]] = 2;
+                    board[prev_move][Performance[j]] = player;
+                    if(isWon(board[Performance[j]],!player,2)!=1){
+                        board[prev_move][Performance[j]] = 2;
+                        printf("2player:%d\n",player);
+                        return Performance[j];
                     }
+                    board[prev_move][Performance[j]] = 2;
                 }
             }
         }
-        printf("3X\n");
-        return PerformanceOfX[PerformanceOfX[0]];
+        printf("3player:%d\n",player);
+        return Performance[Performance[0]];
     }
 }
 
